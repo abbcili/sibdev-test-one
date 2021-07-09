41d55875c0447bab1b4bc30294cedd688a1c897f
Artur Nikolaenko <artur_brylyakov@mail.ru>
2021-07-10 02:28:29 +0700

    added webpack.config.js file

---
+++ webpack.config.js
@@ -0,0 +1,56 @@
+const path = require('path');
+const HTMLWebpackPlugin = require('html-webpack-plugin');
+const {CleanWebpackPlugin} = require('clean-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+
+const isDev = process.env.NODE_ENV === 'development';
+const isProd = !isDev;
+
+console.log('development is ', isDev)
+module.exports = {
+    mode: 'development',
+
+    entry: {
+        main: path.resolve(__dirname, './src/index.js'),
+    },
+
+    output: {
+        path: path.resolve(__dirname, './dist'),
+        filename: '[name].[contenthash].js',
+    },
+
+    resolve: {
+        extensions: ['.js', '.scss',],
+    },
+
+    devServer: {
+        port: 4200,
+    },
+
+    devtool: isDev ? 'source-map' : false,
+
+    plugins: [
+        new CleanWebpackPlugin(),
+        new HTMLWebpackPlugin({
+            template: path.resolve(__dirname, './src/index.html'),
+            filename: 'index.html',
+        }),
+        new MiniCssExtractPlugin({
+            filename: '[name].[contenthash].css',
+        }),
+    ],
+
+    module: {
+        rules: [
+            {
+                test: /\.(?:ico|gif|png|jpg)$/i,
+                type: 'asset/resource',
+            },
+            {
+                test: /\.(scss|css)$/,
+                use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader'],
+            },
+        ]
+    }
+}
